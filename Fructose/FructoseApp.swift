9d8cfea9971585988454cb8c27a166a71cc97dd4
Brian Lee <brian.ts.lee.0907@gmail.com>
2021-06-11 18:16:42 +0900

    Initial Commit

---
+++ Fructose/FructoseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructoseApp.swift
+//  Fructose
+//
+//  Created by Brian Lee on 2021/06/11.
+//
+
+import SwiftUI
+
+@main
+struct FructoseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
